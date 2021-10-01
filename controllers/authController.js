77cd40fcd62ed4dfdaeefeff041908dffe219aba
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-10-01 19:55:43 -0300

    Panel de admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport')
+const mongoose=require('mongoose')
+const Vacante = mongoose.model('Vacante')
 
 exports.autenticarUsuario =  passport.authenticate('local',{
     successRedirect:'/administracion',
@@ -19,9 +21,15 @@ exports.verificarUsuario= (req,res,next)=>{
     res.redirect('/iniciar-sesion')
 }
 
-exports.mostrarPanel=(req,res,next)=>{
+exports.mostrarPanel=async (req,res,next)=>{
+
+    //consultar el usuario autenticado
+    const vacantes = await Vacante.find({autor:req.user._id}).lean();
+
+
     res.render('administracion',{
         nombrePagina: 'Panel de Administracion',
-        tagline:'Crea y administra tus vacantes desde aqui'
+        tagline:'Crea y administra tus vacantes desde aqui',
+        vacantes
     })
 }
