a798dd1a62bf5298257b5317cfab746b95501f0b
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-10-01 19:19:21 -0300

    Creand admin panel autenticacion y las rutas

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,22 @@ exports.autenticarUsuario =  passport.authenticate('local',{
     failureFlash:true,
     badRequestMessage:'Ambos campos son obligatorios'
 })
+
+//revisar si el usuario esta autendicado o no
+
+exports.verificarUsuario= (req,res,next)=>{
+    //revisar el usuario
+    if(req.isAuthenticated()){
+   
+        return next()//estan autenticados
+    }
+    //caso contrario redireccionar
+    res.redirect('/iniciar-sesion')
+}
+
+exports.mostrarPanel=(req,res,next)=>{
+    res.render('administracion',{
+        nombrePagina: 'Panel de Administracion',
+        tagline:'Crea y administra tus vacantes desde aqui'
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,33 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/homeController");
 const vacantesController = require("../controllers/vacantesController");
-const usuariosController= require ('../controllers/usuariosController');
-const autenticarUsuario  = require("../controllers/authController");
+const usuariosController = require("../controllers/usuariosController");
+const authController = require("../controllers/authController");
 
 module.exports = () => {
   router.get("/", homeController.mostrarTrabajos);
 
   //crear vacantes
-  router.get("/vacantes/nueva", vacantesController.formularioNuevaVacante);
-  router.post("/vacantes/nueva", vacantesController.agregarVacante);
+  router.get("/vacantes/nueva",authController.verificarUsuario, vacantesController.formularioNuevaVacante);
+  router.post("/vacantes/nueva",authController.verificarUsuario,  vacantesController.agregarVacante);
 
   //mostrar vacante
   router.get("/vacantes/:url", vacantesController.mostrarVacante);
 
-  router.get("/vacantes/editar/:url", vacantesController.formEditarVacante);
-  router.post("/vacantes/editar/:url", vacantesController.editarVacante);
+  router.get("/vacantes/editar/:url",authController.verificarUsuario,  vacantesController.formEditarVacante);
+  router.post("/vacantes/editar/:url",authController.verificarUsuario,  vacantesController.editarVacante);
 
-  router.get('/crear-cuenta',
-    usuariosController.formCrearCuenta
-  )
-  router.post('/crear-cuenta',
+  router.get("/crear-cuenta", usuariosController.formCrearCuenta);
+  router.post(
+    "/crear-cuenta",
     usuariosController.validarResgistro,
     usuariosController.crearUsuario
-  )
-//autenticar usuario
-router.get('/iniciar-sesion',
-  usuariosController.formIniciarSesion
-  )
-  router.post('/iniciar-sesion',
-  autenticarUsuario.autenticarUsuario
-  )
+  );
+  //autenticar usuario
+  router.get("/iniciar-sesion", usuariosController.formIniciarSesion);
+  router.post("/iniciar-sesion", authController.autenticarUsuario);
+  //panel de administracinh
+  router.get("/administracion", authController.verificarUsuario, authController.mostrarPanel);
 
   return router;
 };
